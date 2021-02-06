7ac71fed6bc74fcd4b332c1a00e3d460536e7685
naza <nazari_93@hotmail.com>
2021-02-06 20:10:37 +0100

    clase casa 05/02/2021

---
+++ MvcCoreNuevo/Controllers/CochesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreNuevo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreNuevo.Controllers
+{
+    public class CochesController : Controller
+    {
+        private List<Coche> GetCoches()
+        {
+            List<Coche> coches = new List<Coche>();
+            Coche car = new Coche
+            {
+                IdCoche = 1,
+                Marca = "Ford",
+                Modelo = "Mustang",
+                VelocidadMaxima = 320
+            };
+            coches.Add(car);
+            car = new Coche
+            {
+                IdCoche = 2,
+                Marca = "Seat",
+                Modelo = "600",
+                VelocidadMaxima = 95
+            };
+            coches.Add(car);
+            car = new Coche
+            {
+                IdCoche = 3,
+                Marca = "Fiat",
+                Modelo = "Panda",
+                VelocidadMaxima = 105
+            };
+            coches.Add(car);
+            car = new Coche
+            {
+                IdCoche = 4,
+                Marca = "Opel",
+                Modelo = "Astra",
+                VelocidadMaxima = 140
+            };
+            coches.Add(car);
+            car = new Coche
+            {
+                IdCoche = 5,
+                Marca = "Nissan",
+                Modelo = "Kaskai",
+                VelocidadMaxima = 200
+            };
+            coches.Add(car);
+            return coches;
+        }
+        public IActionResult Index()
+        {
+            return View(this.GetCoches());
+        }
+
+        public IActionResult CochesAsincronos()
+        {
+            return View();
+        }
+
+        public IActionResult GetCochesPartial()
+        {
+            List<Coche> coches = this.GetCoches();
+            //Busca en su carpeta de controller (Coches) o en Shared
+            return PartialView("_CochesPartial", coches);
+        }
+
+        public IActionResult GetDetallesPartial(int id) //tiene que llamarse id por el
+                                                        //mapa de rutas del startup
+        {
+            Coche car = this.GetCoches().SingleOrDefault(z => z.IdCoche == id);
+            return PartialView("_DetallesCochePartial", car);
+        }
+    }
+}
---
+++ MvcCoreNuevo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreNuevo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogIn(String username)
+        {
+            HttpContext.Session.SetString("username", username);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CloseSession()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
